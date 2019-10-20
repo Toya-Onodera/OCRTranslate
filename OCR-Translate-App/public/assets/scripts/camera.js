90369de76abc046e7698778f41033403ceaf8427
Toya-Onodera <gtrzexel@gmail.com>
2019-10-20 23:05:20 +0900

    とりあえず、バックアップ (Air を初期化するので。作業内容は忘れてしまった)

--- OCR-Translate-App/public/assets/scripts/camera.js
+++ OCR-Translate-App/public/assets/scripts/camera.js
@@ -1,1 +1,8 @@
 const socket = io()
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector('canvas#camera')
+    const ctx = canvas.getContext('2d')
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+    
+})
